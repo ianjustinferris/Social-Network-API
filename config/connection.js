f0f7f09be4cb736cb7a2d7f8a86e8620bf19e101
ianjustinferris <ianjustinferris@gmail.com>
2022-07-08 21:34:19 -0400

    Basic file structure. Rename files

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const { connect, connection } = require('mongoose');
+
+// Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
---
+++ models/index.js
@@ -0,0 +1,4 @@
+const User = require('./user');
+const Thought = require('./thought');
+
+module.exports = { User, Thought };
