19e423bc0c35c402c9e652a8b041c66830f9c2c2
ianjustinferris <ianjustinferris@gmail.com>
2022-07-09 12:27:17 -0400

    User Model

--- models/user.js
+++ models/user.js
@@ -1,34 +1,35 @@
-const { Schema, Types } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const assignmentSchema = new Schema(
+const userSchema = new Schema(
   {
-    assignmentId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
-    assignmentName: {
+    email: {
       type: String,
       required: true,
-      maxlength: 50,
-      minlength: 4,
-      default: 'Unnamed assignment',
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address"
+      ]
     },
-    score: {
-      type: Number,
-      required: true,
-      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-  },
-  {
-    toJSON: {
-      getters: true,
-    },
-    id: false,
-  }
-);
 
-module.exports = assignmentSchema;
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }]
+
+  });
+
+
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
