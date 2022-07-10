e548b769dcf7fabb4948271ab82a98d89c6fecba
ianjustinferris <ianjustinferris@gmail.com>
2022-07-09 21:55:45 -0400

    User routes updated

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,20 +1,37 @@
 const router = require('express').Router();
 const {
-  getCourses,
-  getSingleCourse,
-  createCourse,
-  updateCourse,
-  deleteCourse,
-} = require('../../controllers/courseController.js');
+  getAllUsers,
+  getUserById,
+  postNewUser,
+  putUserById,
+  deleteUserById,
+  postNewFriend,
+  deleteFriend
+} = require('../../controllers/userController.js');
 
-// /api/courses
-router.route('/').get(getCourses).post(createCourse);
-
-// /api/courses/:courseId
+// /api/users
 router
-  .route('/:courseId')
-  .get(getSingleCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
+  .route('/')
+  //GET all users
+  .get(getAllUsers)
+  //POST a new user 
+  .post(postNewUser);
+
+// /api/users/:userId
+router
+  .route('/:id')
+  //GET a single user by its _id and populated thought and friend data
+  .get(getUserById)
+  //PUT to update a user by its _id
+  .put(putUserById)
+  //DELETE to remove user by its _id
+  .delete(deleteUserById);
+
+router
+  .route('/:userId/friends/:friendId')
+  //POST to add a new friend to a user's friend list
+  .post(postNewFriend)
+  //DELETE to remove a friend from a user's friend list 
+  .delete(deleteFriend);
 
 module.exports = router;
